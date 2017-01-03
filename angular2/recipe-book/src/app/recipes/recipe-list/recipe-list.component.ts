83f87bb2c893cf2cb0efbadd9185f035368fb1f0
alex <adoundakov@gmail.com>
2017-01-03 10:31:14 -0800

    add recipe detail component, binding to load component

--- angular2/recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
+++ angular2/recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe';
 import { AppComponent } from '../../app.component';
 
@@ -8,10 +8,15 @@ import { AppComponent } from '../../app.component';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [];
+  @Output() recipeSelected = new EventEmitter<Recipe>();
   recipe = new Recipe('Dummy', 'Dummy', 'https://cdn0.iconfinder.com/data/icons/kameleon-free-pack-rounded/110/Food-Dome-128.png');
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSelected(recipe: Recipe) {
+    this.recipeSelected.emit(recipe);
   }
 
 }
