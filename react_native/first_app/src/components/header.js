dbf7c569460edf468caac910bd31efa21ab97de6
Alex Doundakov <adoundakov@gmail.com>
2017-01-26 19:28:39 -0800

    add header styling

--- react_native/first_app/src/components/header.js
+++ react_native/first_app/src/components/header.js
@@ -1,15 +1,30 @@
 // Import creative libraries
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
 // Make component
 const Header = () => {
-  return <Text>Albums!</Text>;
+  const { textStyle, viewStyle } = styles;
+
+  return (
+    <View style={viewStyle}>
+      <Text style={textStyle}>Albums!</Text>
+    </View>
+  );
 };
 
 // Make Styling
 const styles = {
-  
+  textStyle: {
+    fontSize: 20
+  },
+  viewStyle: {
+    backgroundColor: '#F8F8F8',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 60,
+    paddingTop: 15
+  }
 };
 
 // Make Component available to other parts of the app
